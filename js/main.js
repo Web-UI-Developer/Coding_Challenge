0ea1101791244970fc87cba1078ae942662ebe33
SAMPREET.NAIK <sampreet.github@gmail.com>
2017-10-14 01:01:09 -0400

    JavaScript commit with given var and expected output in console.

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+var containerHeight = 720;
+var containerWidth = 600;
+
+var scheduledEvents = function(events) {
+
+}
+
+window.layOutDay = function(events) {
+    
+  }
+
+ // invoke from the console for testing purposes 
+  window.onload = function() {
+    layOutDay([
+      {start: 30, end: 150},
+      {start: 540, end: 600},
+      {start: 560, end: 620},
+      {start: 610, end: 670}
+    ])
+  }
